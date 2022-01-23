8e15bf0710b7aff50d0b2a60d52a9b53473966b5
Rajiv Gupta <rajivit53@gmail.com>
2022-01-23 13:49:19 +0530

    learn spring security basics
    #First commit - spring security basic web application.

---
+++ src/main/java/com/eazybytes/springsecutirybasics/SpringsecutirybasicsApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecutirybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecutirybasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecutirybasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazybytes/springsecutirybasics/SpringsecutirybasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecutirybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecutirybasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
